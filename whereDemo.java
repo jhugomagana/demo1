6e258dfe157c40860d967c7c8dcc398ce7a300cd
J Hugo <84883798+jhugomagana@users.noreply.github.com>
2022-02-04 17:51:13 -0800

    fill in demo code - class file & JUnit

--- whereDemo.java
+++ whereDemo.java
@@ -1,1 +1,16 @@
-//copy and paste in vsCode
+import java.io.IOException;
+import java.nio.file.Path;
+
+class WhereAmI {
+	
+    public static String thisDirectory() {
+        String myDir = System.getProperty("user.dir");
+        return myDir;
+
+    }
+    
+    public static void main(String[] args){
+
+        System.out.print(thisDirectory());
+    }
+}
