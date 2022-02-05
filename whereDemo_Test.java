5211de4b6d53d699a5d27346f1f68e951b45229d
J Hugo <84883798+jhugomagana@users.noreply.github.com>
2022-02-04 17:55:48 -0800

    update lib file - update JUnit file

--- whereDemo_Test.java
+++ whereDemo_Test.java
@@ -1,3 +1,15 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+// windows MINGW64 bash commands
+// javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" whereDemo_Test.java
+// java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore whereDemo_Test
+
 public class whereDemo_Test {
-    
+    @Test
+    public void addition() {
+        assertEquals(5, 1 + 1);
+    }
+
+
 }
